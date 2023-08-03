44817f87f51691b979ae347ffed69f16be8626a2
Alderinn <dmancini199@gmail.com>
2023-08-02 20:27:44 -0400

    Finished yaml engine block

--- yamlengine.py
+++ yamlengine.py
@@ -1,11 +1,8 @@
 # testing space for reading nad displaying yaml files
 # Aug 2, 2023 20:05
-
-# import pyyaml module
-import pyyaml
-from pyyaml.loader import SafeLoader
-
+import yaml
+from yaml.loader import SafeLoader
 # Open the file and load the file
-with open('Userdetails.yaml') as f:
+with open("entry.yml") as f:
     data = yaml.load(f, Loader=SafeLoader)
     print(data)
