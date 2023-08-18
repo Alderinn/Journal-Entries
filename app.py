3f540a34e7e8afe6a77f13514a6125469d122bab
Alderinn <dmancini199@gmail.com>
2023-08-18 16:27:20 -0400

    trying to get the list entries to work (and struggling)

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ from datetime import date
 from journal import writeNewFile
 from tkcalendar import Calendar
 from tkinter import ttk
+import os
 
 
 class CalendarW(ctk.CTkCanvas): #TODO: -Not functioning
@@ -29,16 +30,30 @@ class CalendarW(ctk.CTkCanvas): #TODO: -Not functioning
                     selectbackground=ctk.ThemeManager.theme["CTkButton"]["fg_color"][1])
         cal.pack(fill="both", expand=True, padx=10, pady=10)
         print(cal)
+
 class EntryResults(ctk.CTkScrollableFrame):
     def __init__(self, master, **kwargs):
         super().__init__(master, **kwargs)
         #TODO: Stopped here
-        entries = ["Entry1","Entry2","Entry3"]
+
+    def GetEntries():
+        os.chdir("." ) #uses os.walk to "walk" from the root directory to the entries folder
+        directory = './entries'
+        for filename in os.listdir(directory):
+            f = os.path.join(directory, filename)
+            # checking if it is a file
+            if os.path.isfile(f):
+                print(f)
+                entries = [str(f)[:-4]]
+                print(f"Entries: {f}")
         r=0
         for e in entries:
-            self.label=ctk.CTkLabel(self,text=e)
-            self.label.grid(row=[r],column=1,)
+            print("MADE IT TO e in ENTRIES")
+            print(f"e = {e}")
             r = r+1
+            MyFrame.label=ctk.CTkLabel(App,text=e)
+            MyFrame.label.grid(row=[r],column=1,)
+            
 
 class MyFrame(ctk.CTkFrame):
     def __init__(self, master):
@@ -56,10 +71,10 @@ class MyFrame(ctk.CTkFrame):
         self.button = ctk.CTkButton(self, text="Make an Entry 📝", command=self.WipeOpenEntry, width=220, height=40, fg_color='#0083aa',font=('',28))
         self.button.grid(row=1, column=0, padx=20, pady=(50,0), columnspan=2, ipadx=10,ipady=5)
         # Entry Search
-        self.button = ctk.CTkButton(self, text="Entry Search 🔍", command=self.ShowPage, width=220, height=40, fg_color='#004155',font=('',28))
+        self.button = ctk.CTkButton(self, text="Entry Search 🔍",command=self.WipeList,width=220, height=40, fg_color='#004155',font=('',28))
         self.button.grid(row=2, column=0, padx=20, pady=5, columnspan=2, ipadx=10,ipady=5)
         # All Entries
-        self.button = ctk.CTkButton(self, text="List ALL", command=quit, width=220, height=40, fg_color='#004155',font=('',28))
+        self.button = ctk.CTkButton(self, text="List ALL", command=self.WipeList, width=220, height=40, fg_color='#004155',font=('',28))
         self.button.grid(row=3, column=0, padx=20, pady=0, columnspan=2, ipadx=10,ipady=5)
         # EXIT
         self.button = ctk.CTkButton(self, text="EXIT", command=quit, width=80, height=20, fg_color='red')
@@ -67,15 +82,21 @@ class MyFrame(ctk.CTkFrame):
 
     # - Wipe the frame, then populate with new content
     def WipeOpenEntry(self):
-        print("button pressed")
+        print("Atempting to display Enter Entry....")
         for widget in MyFrame.winfo_children(self):
             widget.destroy()
         self.EntryPage()
+
     def WipeSearch(self):
-        print("button pressed")
+        print("Atempting to display Entry Search....")
         for widget in MyFrame.winfo_children(self):
             widget.destroy()
         self.SearchPage()
+    def WipeList(self):
+        print("Atempting to display List of Entries....")
+        for widget in MyFrame.winfo_children(self):
+            widget.destroy()
+        EntryResults.GetEntries()
 
 
 
@@ -90,6 +111,7 @@ class MyFrame(ctk.CTkFrame):
     def ShowPage(self):
         self.my_frame = EntryResults(master=self)
         self.my_frame.grid(row=0, column=0, padx=20, pady=20, sticky="nsew")
+    
     # Entry Page
     def EntryPage(self):
         self.grid_columnconfigure((0,1), weight=1)
@@ -106,7 +128,7 @@ class MyFrame(ctk.CTkFrame):
         self.entry2.grid(row=0,column=0,sticky='e',padx=(0,10),pady=(30,0),)
         
         # BODY TEXTBOX
-        self.textbox = ctk.CTkTextbox(master=self, width=400, corner_radius=10)
+        self.textbox = ctk.CTkTextbox(master=self, width=400, corner_radius=10,activate_scrollbars=True)
         self.textbox.grid(row=1, column=0, sticky="ew",columnspan=2, padx=20,pady=20 )
         self.textbox.insert("0.0", "")
 
@@ -168,6 +190,8 @@ class App(ctk.CTk):
         self.grid_columnconfigure((0), weight=1)
         self.frame1 = MyFrame(self)
         self.frame1.grid(row=0, column=0, padx=10, pady=(0), sticky='nwes')
+    def test(self):
+        print("Does this run ===============")
 
 app = App()
 app.mainloop()
